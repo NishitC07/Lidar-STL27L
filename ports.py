8bbbdb2c92af3ffb6dcfb418ce7d892aeb0a954e
NishitC07 <chaudharynishit56@gmail.com>
2024-09-29 01:37:30 +0530

    Last Commit

--- ports.py
+++ ports.py
@@ -4,11 +4,8 @@ import serial.tools.list_ports
 
 def list_com_ports():
     ports = serial.tools.list_ports.comports()
-    if not ports:
-        print("No COM ports found.")
-    else:
-        print("Available COM ports:")
-        for port in ports:
+    
+    for port in ports:
             print(f"{port.device} - {port.description}")
 
 if __name__ == "__main__":
